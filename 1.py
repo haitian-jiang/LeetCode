142a76a273f1c247c97b93bd6a92df61f6ef2253
bill-ginger <jht2001120@163.com>
2020-04-19 13:34:47 +0800

    problem 1

---
+++ 1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        length = len(nums)
+        answer = []
+        for i in range(length): # 遍历查找解
+            for j in range(i+1, length): #避免重复找解
+                if nums[i]+nums[j] == target:
+                    answer.append(i)
+                    answer.append(j)
+                    return answer
