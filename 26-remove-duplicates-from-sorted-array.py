5c073119da93ff7c856ded96fc2de95b32b7d1b1
bill-ginger <jht2001120@163.com>
2020-04-23 16:45:50 +0800

    easy

---
+++ 26-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if(nums == []):
+            return 0
+        index = 0 # 从头开始删
+        while(True):
+            if(index == len(nums)-1): #最后一个元素时跳出循环
+                break
+            if(nums[index] == nums[index+1]): # 下一个元素一样，那就删掉这个元素，后面的自动到前面，所以索引不变
+                nums.pop(index)
+                continue
+            else: # 下一个元素不一样，那就继续循环，查后面的
+                index += 1
+        return len(nums)
