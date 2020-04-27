ac42ef0542e6c79dbf8c9395f1b98f6d69616ae7
bill-ginger <jht2001120@163.com>
2020-04-27 23:23:08 +0800

    easy

--- 58.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        ls = s.split() # 转存为列表
-        if ls == []: # s为空串或只有空格
-            return 0
-        if ls[-1].isalpha(): # 判断是否是单词
-            return len(ls[-1])
-        else: # 否则为符号或数字
-            return 0
