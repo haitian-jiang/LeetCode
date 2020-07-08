6a371f8b96ac65c1c5027e510eb9e164843f0c23
bill-ginger <jht2001120@163.com>
2020-07-08 12:07:46 +0800

    update

--- 192-word-frequency.sh
+++ 192-word-frequency.sh
@@ -1,2 +1,3 @@
-# Read from the file words.txt and output the word frequency list to stdout.
+# medium
+# 2020-07-05
 awk '{for(i=1;i<=NF;i++)dict[$i]+=1}END{for(i in dict)print(i,dict[i])}' words.txt | sort -nrk 2,2
--- 193-valid-phone-numbers.sh
+++ 193-valid-phone-numbers.sh
@@ -1,2 +1,3 @@
-# Read from the file file.txt and output all valid phone numbers to stdout.
+# easy
+# 2020-07-05
 grep -E '(^\([0-9]{3}\)\ [0-9]{3}-[0-9]{4}$|^[0-9]{3}-[0-9]{3}-[0-9]{4}$)' file.txt
