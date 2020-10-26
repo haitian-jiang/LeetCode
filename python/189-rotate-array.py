d84bdf541d565cdc68058dc821e8752aa14b7dca
bill-ginger <jht2001120@163.com>
2020-10-26 15:40:47 +0800

    rotate_array

---
+++ python/189-rotate-array.py
@@ -0,0 +1,13 @@
+# medium
+from typing import List
+
+'''2020-10-26'''
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        nums.reverse()
+        nums[:k] = nums[:k][::-1]
+        nums[k:] = nums[k:][::-1]
