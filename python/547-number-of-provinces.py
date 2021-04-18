ccb537f1579fe62392385b501ae4bd02a4b238f9
bill-ginger <jht2001120@163.com>
2021-04-18 23:17:30 +0800

    initial

--- python/547-number-of-provinces.py
+++ python/547-number-of-provinces.py
@@ -21,3 +21,47 @@ class Solution:  # DFS
                 prov_amt += 1
                 self.dfs(isConnected, city)
         return prov_amt
+
+
+'''2020-01-29'''
+class City:
+    def __init__(self, num):
+        self.parent = num
+        self.count = 1
+
+
+class Solution:  # union-find set
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        city_amt = len(isConnected)
+        cities = [City(i) for i in range(city_amt)]
+        def find(x):
+            y = x
+            while cities[y].parent != y:
+                y = cities[y].parent
+            while x != y:
+                t = cities[x].parent
+                cities[x].parent = y
+                x = t
+            return y
+        def union(x, y):
+            x = find(x)
+            y = find(y)
+            if x == y:
+                return
+            if cities[x].count > cities[y].count:
+                cities[y].parent = x
+                cities[x].count += cities[y].count
+            else:
+                cities[x].parent = y
+                cities[y].count += cities[x].count
+
+        for i in range(city_amt):
+            for j in range(i, city_amt):
+                if isConnected[i][j]:
+                    union(i, j)
+
+        prov_amt = 0
+        for i in range(city_amt):
+            if cities[i].parent == i:
+                prov_amt += 1
+        return prov_amt
