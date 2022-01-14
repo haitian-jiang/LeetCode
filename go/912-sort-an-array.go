c718278c761b588d9af14e0fe6fbd102788f5514
bill-ginger <jht2001120@163.com>
2022-01-14 11:27:58 +0800

    go

---
+++ go/912-sort-an-array.go
@@ -0,0 +1,37 @@
+// medium
+// 2022-01-14
+func sortArray(nums []int) []int {
+    // MergeSort
+    if len(nums) <= 1 {
+        return nums
+    }
+    leftHalf := sortArray(nums[:len(nums)/2])
+    rightHalf := sortArray(nums[len(nums)/2:])
+    i, j, k := 0, 0, 0
+    result := make([]int, len(nums))
+    for i < len(leftHalf) && j < len(rightHalf) {
+        if leftHalf[i] <= rightHalf[j] {
+            result[k] = leftHalf[i]
+            i++
+            k++
+        } else {
+            result[k] = rightHalf[j]
+            j++
+            k++
+        }
+    }
+    if i < len(leftHalf) {
+        for k < len(nums) {
+            result[k] = leftHalf[i]
+            i++
+            k++
+        }
+    } else if j < len(rightHalf) {
+        for k < len(nums) {
+            result[k] = rightHalf[j]
+            j++
+            k++
+        }
+    }
+    return result
+}
