b013f315d5071fe83c4818fd534f2bc8987f7f67
bill-ginger <jht2001120@163.com>
2022-01-15 10:05:21 +0800

    3

--- python/3-longest-substring-without-repeating-characters.py
+++ python/3-longest-substring-without-repeating-characters.py
@@ -14,3 +14,18 @@ class Solution:  # O(n)
                     max_len = max(max_len, len(window))
                     break
         return max_len
+
+'''2022-01-15'''
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s) < 2:
+            return len(s)
+        left = right = max_len = 0
+        window = set()
+        while left < len(s):
+            while right < len(s) and s[right] not in window:
+                window.add(s[right])
+                max_len = max(max_len, len(window))
+                right += 1
+            window.discard(s[left])
+            left += 1
+        return max_len
